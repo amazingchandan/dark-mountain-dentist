292535b6a5407fe7438886b9e072897615b09a5c
a14a19 <anas31197@gmail.com>
2023-03-06 16:45:58 +0530

    UI changes

--- src/app/views/pages/mark-xray/mark-xray.component.ts
+++ src/app/views/pages/mark-xray/mark-xray.component.ts
@@ -7,6 +7,17 @@ import Swal from 'sweetalert2';
 })
 export class MarkXrayComponent {
 
+  // is equal to default value of input range
+  valInput = '25';
+  leftPos = `25%`;
+
+  onRangeChange(event: any){
+    console.log(event);
+    this.valInput = (<HTMLInputElement>event.target).value.trim();
+    console.log((<HTMLInputElement>event.target).value.trim());
+    this.leftPos = `${+(<HTMLInputElement>event.target).value.trim() - 5}%`
+  }
+
   ngOnInit(){
 
   }
--- src/app/views/pages/set-new-password/set-new-password.component.ts
+++ src/app/views/pages/set-new-password/set-new-password.component.ts
@@ -6,5 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./set-new-password.component.scss']
 })
 export class SetNewPasswordComponent {
-
 }
