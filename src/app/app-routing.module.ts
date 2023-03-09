bf94eb26bd6074f53aeb474aef3aba3af7e45db1
haripriya <haripriya27@gmail.com>
2023-03-09 16:06:41 +0530

    xray list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -147,7 +147,7 @@ const routes: Routes = [
   },
   {
     path: 'dashboard',
-    component: DefaultLayoutComponent,
+    component: DefaultLayoutComponent, 
     data: {
       title: 'Dashboard'
     },
