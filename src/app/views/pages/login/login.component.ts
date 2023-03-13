b9f5cce2dad8be6fa9c7142f8de8f39bafaf7bcc
haripriya <haripriya27@gmail.com>
2023-03-13 11:11:18 +0530

    bug solve1

--- src/app/views/pages/login/login.component.ts
+++ src/app/views/pages/login/login.component.ts
@@ -55,9 +55,10 @@ export class LoginComponent implements OnInit, OnDestroy {
         password: this.loginForm.get("password").value
       };
      this.apiService.onLogin(JSON.stringify(loginData)).subscribe((result: any) => {
-        console.log(result.userInfo.id);
-        let id= result.userInfo.id;
+       // console.log(result);
+       // let id= result.userInfo.id;
         if (result.success) {
+          let id= result.userInfo.id;
           this.apiService.getUserRecordById(id).subscribe((res: any) => {
             console.log(res,"*****");
              if(res.getData[0]?.role=='dentist'){
