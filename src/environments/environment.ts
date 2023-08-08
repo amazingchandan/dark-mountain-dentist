ad1e7c45c76b0baef06a459d9199ebe7b2a20fa3
a14a19 <anas31197@gmail.com>
2023-08-08 18:56:59 +0530

    changes made

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -13,8 +13,6 @@ export const environment = {
   PAYPAL_CLIENT_ID: 'AeKffQqEC4lR2FtZBUdTIlOz6vMXajfBakTU2IIqdmA18KxLwV7FHpfMagXrAqf0RAwc7evqE3_HcvKr',
   PAYPAL_CLIENT_SECRET_KEY: 'EPNEGNEQmmqoQ3-Re3U7gyVkH3jIPS1h8Ai_mti1fBdMwkpIu2GeQxqFxg3Oy4JetoMQM-PLMK4yjBLU',
   PROD_ID: 'PROD-6X2846590V484010V',
-  // API_HOST : https://apidarkmountain.blahworks.tech/,
-  //  API_HOST: 'https://apidarkmountain.blahworks.tech/',
   // ! google recaptcha
   recaptcha: {
     siteKey: '6Ld7TU0mAAAAAFjH7axIGjhz4hCyTVTkAzGOtUfs'
